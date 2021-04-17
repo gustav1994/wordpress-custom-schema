1544f25fb46953c4a87e2e067d8acb84cfafff2d
Gustav Svendsen <gustav@salus.group>
2021-04-17 20:30:55 +0200

    Init commit

---
+++ wordpress-custom-schema.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * Plugin Name: Wordpress Custom Schema     
+     * Description: Use this plugin to create custom fields and associate them with custom post types
+     * Version: 1.0
+     * Author: Gustav Svendsen
+     * Author URI: https://gustavs.dk     
+     */
+
+    
+
+    
