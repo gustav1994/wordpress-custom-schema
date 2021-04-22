4e93160203c0afbd875fbbd4d5d53f88543edd02
Gustav Svendsen <gustav@salus.group>
2021-04-22 20:40:58 +0200

    Function for building a bootstrap grid

--- WCS_Group.php
+++ WCS_Group.php
@@ -11,7 +11,7 @@
          *
          * @var [type]
          */
-        protected $key;
+        public $key;
 
         /**
          * The Meta Box Title
@@ -36,9 +36,7 @@
         public function __construct( string $key )
         {
             if( $this->validateKey($key) ) {
-
                 $this->key = $key;                
-
             }
 
             return $this;
@@ -51,16 +49,144 @@
          */
         public function activate()
         {
-            return add_action("add_meta_boxes", function(){
-                
-                add_meta_box($this->key, $this->name, [$this, "render"], [], 'normal', 'default');        
+            if( function_exists("add_action") ) {
 
-            });
+                return add_action("add_meta_boxes", function(){
+                    
+                    add_meta_box($this->key, $this->name, [$this, "render"], [], 'normal', 'default');        
+
+                });
+
+            }
+
+            return false;
         }
 
-        public function render()
+        /**
+         * Set the end-uset meta box name
+         *
+         * @param string $name
+         * @return void
+         */
+        public function setName( string $name ) 
         {
-            // Render the basic layout and also call each field to render it's content
+            $this->name = $name;
+
+            return $this;
+        }
+
+        /**
+         * Add one or more fields to this group
+         *
+         * @param [type] $fields
+         * @return void
+         */
+        public function addFields( $fields )
+        {
+            $fields = is_array($fields) ? $fields : [$fields];
+
+            foreach( $fields as $field ) {
+                $this->fields[$field->key] = $field;
+            }     
+            
+            // Sort by the start_position ascending
+            usort($this->fields, function($a, $b){  
+                                
+                if( is_numeric($a->start_position) && is_numeric($b->start_position) ) {
+                    return $a->start_position < $b->start_position ? -1 : 1;                              
+                } elseif( is_numeric($a->start_position) && !is_numeric($b->start_position) ) {
+                    return -1;
+                }
+
+                return 1;
+            });            
+
+            return $this;
+        }        
+
+        /**
+         * Render the meta box content by rendering every field in the group into
+         * the bootstrap 12 width grid
+         *
+         * @param [type] $post
+         * @return string
+         */
+        public function render( $post ) : string
+        {
+            $grid = $this->renderGrid(array_map(function($field) use ($post) {
+
+                return [$field->start_position, $field->end_position, $field->render($post)];
+
+            }, $this->fields));
+
+            return "
+                <div class='wp-admin-bootstrap'>                    
+                    {$grid}                
+                </div>
+            ";
+        }
+
+        /**
+         * Take postions and generate the Bootstrap grid.
+         * 
+         * Fields:
+         *      start_postion : Start position in the 12 column width bootstrap grid
+         *      end_position : End position in grid
+         *      length : end_position - start_position
+         * 
+         * Columns can not overlap. Start position determines the order
+         * Columns can not span several lines (limits: 12, 24, 36, 48, 60, 72, 84 ....)
+         * Missing start and end positions will be placed last with a 6-column width         
+         *
+         * @param array $cols : array([start, end, content], [start, end, content], ..., [start, end, content])
+         * @return string
+         */
+        protected function renderGrid( array $positions ) : string
+        {                             
+            $columns = [];
+            $firstEmptyPosition = 1;
+
+            foreach( $positions as $position ) {
+
+                $startPosition = intval($position[0]);
+                $endPosition = intval($position[1]);
+                $content = strval($position[2]);
+
+                // Push cols to avoid overlap
+                if( $startPosition < $firstEmptyPosition ) {
+                    $startPosition = $firstEmptyPosition;
+                }
+
+                // Generate offset-cols if necesary
+                if( $startPosition > $firstEmptyPosition ) {
+
+                    for( $i = 0; $i < $startPosition - $firstEmptyPosition; $i++){
+                        $columns[] = "<div class='col-md-1'>&nbsp;</div>";
+                    }
+                    
+                }
+
+                $row = floor($startPosition / 12) + 1;
+
+                // Column should not break lines
+                if( $endPosition > $row * 12) {
+
+                    $endPosition = $row * 12;
+
+                }
+
+                $length = 1 + $endPosition - $startPosition;                
+                
+                $columns[] = "<div class='col-md-{$length}'>{$content}</div>";
+
+                $firstEmptyPosition = $endPosition + 1;
+            }
+
+            return "
+                <div class='row g-4'>
+                    ". implode('', $columns) . "
+                </div>
+            ";
         }
 
         /**
@@ -70,7 +196,7 @@
          * @param string $key
          * @return bool
          */
-        private function validateKey( $key ) : bool
+        protected function validateKey( $key ) : bool
         {
             return is_string($key) && preg_match("/^[a-z\-\_]{1,20}$/", $key);
         }
