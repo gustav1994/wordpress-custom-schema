9f87fc23b4be46284ee73d57f68b22a1eead30ba
Gustav Svendsen <gustav@salus.group>
2021-04-22 16:27:12 +0200

    Set checkbox class

--- fields/WCS_Checkbox.php
+++ fields/WCS_Checkbox.php
@@ -2,12 +2,17 @@
 
     require_once(WP_PLUGIN_DIR . "/wordpress-custom-schema/fields/WCS_Field.php");
 
-    class WCS_Textfield extends WCS_Field
+    class WCS_Checkbox extends WCS_Field
     {
 
         public function render( $post ) : string
         {
-            return "";
+            return "
+                <input type='checkbox' id='{$this->element_id}' class='form-check-input' name='{$this->key}' aria-describedby='{$this->element_id}-help-text' placeholder='{$this->name}'>
+                <label for='{$this->element_id}' class='form-check-label'>{$this->name}</label>                
+                <div id='{$this->element_id}-help-text' class='form-text'>{$this->description}</div>
+                {$this->getNonceField()}
+            ";
         }
 
         public function sanitize( $value )
