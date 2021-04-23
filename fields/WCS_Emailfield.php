f56f93afedf37a004b481e8e78c165dcc7d80a8e
Gustav Svendsen <gustav@salus.group>
2021-04-23 11:07:48 +0200

    Nothing

--- fields/WCS_Emailfield.php
+++ fields/WCS_Emailfield.php
@@ -3,7 +3,7 @@
     require_once(WP_PLUGIN_DIR . "/wordpress-custom-schema/fields/WCS_Field.php");
 
     class WCS_Emailfield extends WCS_Field
-    {
+    {        
         
         public function render( $post ) : string
         {
